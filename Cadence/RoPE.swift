7a3bebc7d65ad921c9077c1a33a4a00072e5943e
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-04-19 21:17:55 +0800

    fix: format the code

--- Cadence/RoPE.swift
+++ Cadence/RoPE.swift
@@ -9,26 +9,30 @@ import Foundation
 import MetalPerformanceShadersGraph
 
 enum RoPE {
-    static func precomputerConsin(seqLen: Int, headDim: Int, base:Float = 10000 ) -> (cos: [Float], sin: [Float]) {
+    static func precomputerConsin(
+        seqLen: Int,
+        headDim: Int,
+        base: Float = 10000
+    ) -> (cos: [Float], sin: [Float]) {
         precondition(headDim % 2 == 0, "headDim must be odd")
         let half = headDim / 2
-        
+
         var theta = [Float](repeating: 0, count: half)
-        for i in 0..<half {
+        for i in 0 ..< half {
             theta[i] = pow(base, -Float(2 * i) / Float(headDim))
         }
-        
+
         var cosTable = [Float](repeating: 0, count: seqLen * headDim)
         var sinTable = [Float](repeating: 0, count: seqLen * headDim)
-        
-        for m in 0..<seqLen {
-            for i in 0..<half {
+
+        for m in 0 ..< seqLen {
+            for i in 0 ..< half {
                 let angle = Float(m) * theta[i]
                 let c = cos(angle)
                 let s = sin(angle)
-                
-                cosTable[m*headDim + i] = c
-                sinTable[m*headDim + i] = s
+
+                cosTable[m * headDim + i] = c
+                sinTable[m * headDim + i] = s
                 cosTable[m * headDim + i + half] = c
                 sinTable[m * headDim + i + half] = s
             }
