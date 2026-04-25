bcfe8c8c5f8c55fc7fb914627e9a9b8e4435c8dd
sakruhnab1 <aruiskyhappy2@outlook.com>
2026-04-25 19:19:04 +0800

    feat: add Vocab test

--- Cadence/CadenceApp.swift
+++ Cadence/CadenceApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct CadenceApp: App {
     init() {
-        ByteShadowMapTest.run()
+        VocabTest.run()
     }
 
     var body: some Scene {
---
+++ Cadence/Test/TokenizerTest/VocabTest.swift
@@ -0,0 +1,24 @@
+//
+//  VocabTest.swift
+//  Cadence
+//
+//  Created by Haotian Gong on 25/4/26.
+//
+
+import Foundation
+
+enum VocabTest {
+    static func run() {
+        guard let vocab = try? Vocab(
+            vocabPath: "/Users/sakruhnab1/Documents/Cadence/Models/Qwen3.5-4B/vocab.json",
+            mergesPath: "/Users/sakruhnab1/Documents/Cadence/Models/Qwen3.5-4B/merges.txt"
+        ) else {
+            fatalError("Failed to read files")
+        }
+
+        print(vocab.tokenToId.count)
+        print(vocab.bpeRanks.count)
+        print(vocab.bpeRanks[Pair(first: "Ġ", second: "Ġ")] as Any)
+        print(vocab.bpeRanks[Pair(first: "i", second: "n")] as Any)
+    }
+}
